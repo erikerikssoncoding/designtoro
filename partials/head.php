3c420760aaf177e24c9ea67391be85562c80226b
erikerikssoncoding <erikeriksson.taicataikalsson@gmail.com>
2025-10-03 12:19:05 +0300

    Enforce 24h form lockout with shared cookie

--- partials/head.php
+++ partials/head.php
@@ -28,7 +28,7 @@ array_unshift($bodyClasses, 'no-js');
 $bodyClassAttribute = htmlspecialchars(implode(' ', array_unique($bodyClasses)), ENT_QUOTES, 'UTF-8');
 ?>
 <!DOCTYPE html>
-<html lang="ro">
+<html lang="ro" class="no-js">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
